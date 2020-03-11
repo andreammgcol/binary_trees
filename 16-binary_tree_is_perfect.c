3ccb99ce6ea86a6c5bb9f75794461673c6bb7ac9
mlgomez0 <mlgomez230@hotmail.com>
2020-03-11 21:41:46 +0000

    create 16-

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,10 +8,11 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int count_perfect = 0, balance;
+	int count_perfect = 0, balance = 0;
 
 	if (tree == NULL)
 		return (0);
+
 	count_perfect = tree_count_perfect(tree);
 	balance = binary_tree_balance(tree);
 	if (count_perfect == 0 && balance == 0)
@@ -83,4 +84,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (height_left);
 	else
 		return (height_rigth);
+}
+
+/**
+ * binary_tree_size - that measures the size  of Binary tree
+ * @tree: pointer to root node of the tree to measure the size
+ * Return: If tree is NULL, your function must return 0
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0, size1 = 0, size2 = 0;
+
+	if (tree == NULL)
+		return (0);
+	size1 = binary_tree_size(tree->left);
+	size2 = binary_tree_size(tree->right);
+	size = size1 + size2;
+	return (size + 1);
 }
